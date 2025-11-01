2361a65cba5344207ee551ebe76603af741440c3
Documenter.jl <documenter@juliadocs.github.io>
2025-11-01 01:57:11 +0000

    build based on 7cf5a2d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#OkCustomInfos","page":"Home","title":"OkCustomInfos","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OkCustomInfos.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
