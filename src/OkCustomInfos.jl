cb867d449abd2c46a74d4d6f07cbd009629e2430
okatsn <okatsn@gmail.com>
2025-11-01 10:10:44 +0800

    Add `infoc`

--- src/OkCustomInfos.jl
+++ src/OkCustomInfos.jl
@@ -1,5 +1,39 @@
 module OkCustomInfos
 
-# Write your package code here.
+using Printf
+
+"""
+    infoc(msg...; prefix="Info: ", prefix_color=:cyan)
+
+Prints a formatted message to the console with a customizable colored prefix.
+
+# Example
+
+```julia
+name = "World"
+
+# Default use (cyan "Info: ")
+infoc("This is the default for %s.", name)
+
+# Your custom "Notice" example
+infoc("This is a blue notice!"; prefix="Notice: ", prefix_color=:blue)
+
+# A red "Error"
+infoc("This is a red error."; prefix="Error: ", prefix_color=:red)
+```
+
+"""
+function infoc(msg...; prefix="Info: ", prefix_color=:cyan)
+    # 1. Print the colored prefix
+    printstyled(prefix; color=prefix_color, bold=true)
+
+    # 2. Format the main message using the passed arguments
+    s = @sprintf(msg[1], msg[2:end]...)
+
+    # 3. Print the message
+    println(s)
+end
+
+
 
 end
